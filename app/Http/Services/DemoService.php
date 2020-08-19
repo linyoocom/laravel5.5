3e519c00aecc4f4e0cfb672e1695bbdbb198cec4
linyoocom <uninput@163.com>
2020-08-19 12:01:39 +0800

    动态规划算法

--- app/Http/Services/DemoService.php
+++ app/Http/Services/DemoService.php
@@ -7,4 +7,38 @@ class DemoService implements TestDemoInterface {
     public function echoText(){
         echo "Demo";
     }
+
+    /**
+     * 动态规划算法
+     */
+    public function dongGui(){
+        $c = [];
+        $i = $j = 0;
+        $N = 10; $T = 1000;
+        for($i=0;$i<$N;$i++){
+            for($j=0;$j<=$T;$j++){
+                $c[$i][$j] = -1;
+            }
+        }
+        return $this->doIt($c,[],[],$i,$j,$T);
+    }
+
+    public function doIt($c,$v,$w,$i,$j,$T){
+        $temp = 0;
+        if($c[$i][$j] != -1){
+            return $c[$i][$j];
+        }
+        if($i==0 || $j == 0){
+            return $c[$i][$j];
+        }else{
+            if($i>0 && $j >= $w[$i]){
+                $temp = $this->doIt($c,$v,$w,$i-1,$j-$w[$i],$T);
+                if($c[$i][$j] < $temp){
+                    //max($c[$i-1][$T-$w[$i]]+$v[$i],$c[$i-1][$T]);
+                    $c[$i][$j] = $temp;
+                }
+            }
+        }
+        return $c[$i][$j];
+    }
 }
