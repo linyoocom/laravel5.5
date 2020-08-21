cfcd982f382f7ef17f49df196e141aabe37e4a12
linyoocom <uninput@163.com>
2020-08-21 10:41:14 +0800

    php artisan 数据库表迁移和数据填充等

--- database/migrations/2020_08_10_094940_create_roles_table.php
+++ database/migrations/2020_08_10_094940_create_roles_table.php
@@ -13,10 +13,23 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('roles')) {
+            //
+            Schema::drop('roles');
+        }
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->increments('role_id');
+            $table->string('name', 30);  //varchar
+            //$table->char('name', 4);
+            $table->tinyInteger('status',false,true);
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->index('name');  //普通索引
         });
+        //执行命令 php artisan migrate --path [目录]
     }
 
     /**
