3ce02dcdc8de2f8c7a61c1b533faaf2e6541d42a
linyoocom <uninput@163.com>
2020-09-11 15:25:16 +0800

    php的几个设计模式

--- app/Http/Services/Decorator/NewTemplate.php
+++ app/Http/Services/Decorator/NewTemplate.php
@@ -15,7 +15,7 @@ class NewTemplate extends TemplateDecorator{
     }
 
     public function render(){
-        $this->newHtml('<img>__text__</img>');   ///在原有结构上增加新功能
+        $this->newHtml('<img>__text__</img>');   ///在原有结构上增加新功能,即已改变了原render方法,适配器模式不会改变原方法.
         $this->_element->render();
     }
 
