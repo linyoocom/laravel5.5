b7acce452869f5ad99cac07773d2622eaaeeb2d8
linyoocom <uninput@163.com>
2021-04-27 09:24:07 +0800

    自定义包用composer方式自动加载

---
+++ storage/TestPackage/TestDo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linyoocom
+ * Date: 2021/4/27
+ * Time: 上午9:01
+ */
+namespace TestPackage;
+
+class TestDo {
+
+    public function doSomething(){
+        echo "0000";
+    }
+}
