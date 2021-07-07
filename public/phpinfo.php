79ba016e5bc0adb4c42efb77aa09aa01cae05a84
linyoocom <uninput@163.com>
2021-07-07 09:45:34 +0800

    ImageMagick将pdf转成jpg

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -1,2 +1,13 @@
 <?php
-phpinfo();
+//phpinfo();
+$im = new \imagick();
+$im->setResolution(150, 150);
+$im->readImage("./207da3015d45f0150f2b1f31fadfd544.pdf");
+$im->setImageFormat('jpeg');
+$im->setImageCompression(\imagick::COMPRESSION_JPEG);
+$im->setImageCompressionQuality(100);
+foreach ($im as $c => $_page) {
+    $_page->writeImage("./test-$c.jpg");
+}
+$im->clear();
+$im->destroy();
